6f6c99aa9bc749fb06b6d8d50ea96359f58df153
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-15 20:02:35 +0800

    feat: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,41 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
+  <Layout title="About | Madeneat Task">
+    <h1>About the task</h1>
+    <h2>Task: </h2>
+    <p>
+      You are required to create a table component from scratch with the
+      following functionalities:
+    </p>
+    <ul>
+      <li>Search</li>
+      <li>Sort</li>
+      <li>Pagination</li>
+    </ul>
+    <p>
+      The use of an Off-the-shelf module is not permitted. Use basic styling.
+      Load the sample.json using fetch. Handle different errors. Code quality
+      and comments will be checked.
+    </p>
+    <strong>Table fields to show:</strong>
+    <ul>
+      <li>Name- (full name), sortable, searchable</li>
+      <li>Gender</li>
+      <li>Amount - sortable</li>
+      <li>Registered - format i.e- Friday 24th February @10:45 PM</li>
+    </ul>
+
+    <span>
+      ** registered current date-time format is in "YYYY-MM-ddThh:mm:ss Z"
+    </span>
     <p>
       <Link href="/">
         <a>Go home</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
