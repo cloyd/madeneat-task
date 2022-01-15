ecc40f5d74611a60dfc122f84f6b6ab2238ccbb5
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-15 19:49:09 +0800

    style: flex nav items

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,21 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import { FC } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import styled from "@emotion/styled";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Nav = styled.nav`
+  display: flex;
+  gap: 1em;
+`;
+
+const Layout: FC<Props> = ({
+  title = "This is the default title",
+  children,
+}) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -15,27 +23,23 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav>
+      <Nav>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        |
         <Link href="/about">
           <a>About</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        |
         <Link href="/users">
           <a>Users List</a>
-        </Link>{' '}
+        </Link>
         | <a href="/api/users">Users API</a>
-      </nav>
+      </Nav>
     </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
