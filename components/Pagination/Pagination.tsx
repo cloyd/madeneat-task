d5eb9850a799543383540c7a2f3f208d7a1c1a61
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-15 19:52:38 +0800

    feat: initialize pagination component

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+import { Container } from "./Pagination.styled";
+
+export const Pagination: FC = () => <Container>Pagination Component</Container>;
+
+export default Pagination;
