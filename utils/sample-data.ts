d125cc3b8fda7bf95716f008262929cd2e01e4c0
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-15 20:15:08 +0800

    feat: dummy users data

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -1,9 +1,33 @@
-import { User } from '../interfaces'
+import { User } from "../interfaces";
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
-]
+  {
+    id: 101,
+    name: "Max Verstappen",
+    gender: "Male",
+    amount: 96,
+    registered: "Friday 24th February @10:45 PM",
+  },
+  {
+    id: 102,
+    name: "Lewis Hamilton",
+    gender: "Male",
+    amount: 95,
+    registered: "Friday 24th February @10:45 PM",
+  },
+  {
+    id: 103,
+    name: "Jane Doe",
+    gender: "Female",
+    amount: 90,
+    registered: "Friday 24th February @10:45 PM",
+  },
+  {
+    id: 104,
+    name: "Test Name",
+    gender: "Female",
+    amount: 100,
+    registered: "Friday 24th February @10:45 PM",
+  },
+];
