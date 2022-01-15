8ee6497d824e960d1493353736c58df5514f5217
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-15 20:07:22 +0800

    refactor: users pages

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,37 +1,34 @@
-import { GetStaticProps } from 'next'
-import Link from 'next/link'
+import { GetStaticProps } from "next";
+import Link from "next/link";
 
-import { User } from '../../interfaces'
-import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
-import List from '../../components/List'
+import { User } from "../../interfaces";
+import { sampleUserData } from "../../utils/sample-data";
+import Layout from "../../components/Layout";
+import List from "../../components/List";
 
 type Props = {
-  items: User[]
-}
+  users: User[];
+};
 
-const WithStaticProps = ({ items }: Props) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
+const WithStaticProps = ({ users }: Props) => (
+  <Layout title="Users List | Madeneat Task">
     <h1>Users List</h1>
     <p>
-      Example fetching data from inside <code>getStaticProps()</code>.
+      Fetching data from inside <code>getStaticProps()</code>.
     </p>
     <p>You are currently on: /users</p>
-    <List items={items} />
+    <List items={users} />
     <p>
       <Link href="/">
         <a>Go home</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const items: User[] = sampleUserData
-  return { props: { items } }
-}
+  const users: User[] = sampleUserData;
+  return { props: { users } };
+};
 
-export default WithStaticProps
+export default WithStaticProps;
