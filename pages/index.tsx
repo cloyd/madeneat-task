42cc2ca97cd8b4b44f8a5da60e869a6c5e8d335b
Cloyd Bilog <cloyddean.bilog@dtn.com>
2022-01-22 22:44:27 +0800

    feat: pagination constants

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Search from "../components/Search";
 import Pagination from "../components/Pagination";
+import usePagination from "../components/Pagination/usePagination";
 
 type Props = {
   users: User[];
@@ -16,6 +17,7 @@ const columns = ["name", "gender", "amount", "registered"];
 
 const HomePage: NextPage<Props> = ({ users: data }) => {
   const [users, setUsers] = useState(data);
+  const { currentPage, setCurrentPage, currentItems } = usePagination(users);
 
   const handleSearch = (searchValue: string): void => {
     if (searchValue !== "") {
@@ -31,8 +33,12 @@ const HomePage: NextPage<Props> = ({ users: data }) => {
   return (
     <Layout title="Users List | Madeneat Task">
       <Search onChange={handleSearch} />
-      <Table data={users} columns={columns} />
-      <Pagination />
+      <Table data={currentItems} columns={columns} />
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        data={users}
+      />
     </Layout>
   );
 };
